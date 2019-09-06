4f962530b98f0146bfe2cc28bfc01c98e80100eb
Steven Santiago <steven.santiago11@upr.edu>
2019-09-06 11:35:03 -0400

    Changed Color of snake, background and grid lines.

--- src/Display/DisplayScreen.java
+++ src/Display/DisplayScreen.java
@@ -34,7 +34,8 @@ public class DisplayScreen {
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        frame.setBackground(Color.black);
+        Color RGBvalue = new Color(128, 0, 128);//Background Color
+        frame.setBackground(RGBvalue);
 
         try {
             frame.setIconImage(ImageIO.read(new File("res/Sheets/icon.png")));
@@ -47,7 +48,7 @@ public class DisplayScreen {
         canvas.setMaximumSize(new Dimension(width, height));
         canvas.setMinimumSize(new Dimension(width, height));
         canvas.setFocusable(false);
-        canvas.setBackground(Color.black);
+        canvas.setBackground(RGBvalue);
 
         frame.add(canvas);
         frame.pack();
