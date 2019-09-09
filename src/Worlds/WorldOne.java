460074c2cb89f2ed3eb555715ff51aa605cd62c3
Steven Santiago <steven.santiago11@upr.edu>
2019-09-09 16:42:17 -0400

    Changed Grid size.

--- src/Worlds/WorldOne.java
+++ src/Worlds/WorldOne.java
@@ -15,8 +15,8 @@ public class WorldOne extends WorldBase{
         super(handler);
 
         //has to be a number bigger than 20 and even
-        GridWidthHeightPixelCount = 40;
-        GridPixelsize = (800/GridWidthHeightPixelCount);
+        GridWidthHeightPixelCount = 60;//
+        GridPixelsize = (800/GridWidthHeightPixelCount);// 800
         playerLocation = new Boolean[GridWidthHeightPixelCount][GridWidthHeightPixelCount];
         appleLocation = new Boolean[GridWidthHeightPixelCount][GridWidthHeightPixelCount];
 
